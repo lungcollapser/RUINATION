5a71078b07fbcc87fc6440093c07f785e4ec04b0
lungcollapser <milesterrell1337@gmail.com>
2025-05-17 22:25:52 -0500

    rev and rep objects kind of work now?
    basically moved from weapon to main but whatever

--- bullet.cpp
+++ bullet.cpp
@@ -6,7 +6,10 @@ bullet::bullet(Vector2 fire_position, int bullet_speed)
 {
 	this->fire_position = fire_position;
 	this->bullet_speed = bullet_speed;
-	active = true;
+
+}
+bullet::bullet()
+{
 
 }
 bullet::~bullet()
@@ -19,6 +22,7 @@ void bullet::draw(Vector2 player_position)
 	if (active)
 	{
 		DrawCircleV(fire_position + player_position, 10, BLUE);
+		DrawRectangle(fire_position.x + player_position.x, fire_position.y + player_position.y, 30, 20, PURPLE);
 	}
 }
 void bullet::update_position()
@@ -27,4 +31,7 @@ void bullet::update_position()
 	fire_position = Vector2Add(fire_position, change_vector);
 
 }
-
+void bullet::bullet_hitbox()
+{
+	
+}
