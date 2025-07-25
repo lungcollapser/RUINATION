ef4eda7cde7ad8c454d252b658d6d9e2c64771d4
lungcollapser <milesterrell1337@gmail.com>
2025-07-24 20:37:24 -0500

    nullptr
    some changes to bullet work

--- weapon.h
+++ weapon.h
@@ -13,10 +13,10 @@ public:
 	~weapon();
 	void fire(Vector2 &weapon_reticle, Vector2 &center_position, int bullet_speed);
 	void draw(Vector2 &player_object);
-	std::unique_ptr<bullet> get_bullets();
+	bullet* get_bullets();
 	Vector2 weapon_reticle, center_position;
 	int bullet_speed, bullet_amount, bullet_damage, max_bullets, clips, current_clips;
-
-	std::unique_ptr<bullet> bullets;
+	
+	bullet* bullets;
 };
 #endif
